a4742ad9c1fba1fdaf4327a8ca5129df0322fd8a
Zhang Xinyu <xinyu.zhang@rea-group.com>
2014-07-30 18:11:23 +0800

    A simple e2e test with protractor

---
+++ test/e2e/juliemr_spec.js
@@ -0,0 +1,6 @@
+describe('angular homepage', function() {
+  it('should have a title', function() {
+    browser.get('http://juliemr.github.io/protractor-demo/');
+    expect(browser.getTitle()).toEqual('Super Calculator');
+  });
+});
---
+++ test/protractor-conf.js
@@ -0,0 +1,4 @@
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['e2e/juliemr_spec.js']
+}
